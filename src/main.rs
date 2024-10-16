5a50096df121bc32630739025a1372566d5fbc70
Illia Khudoshyn <illia.khudoshyn@gmail.com>
2024-10-16 11:18:25 +0100

    Use tracing instead of log

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,26 @@ mod api;
 use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use api::handlers::{create, delete, echo_ws, update, user_by_id, user_types, users};
-use env_logger;
+use tracing::info;
+use tracing_actix_web::TracingLogger;
+use tracing_subscriber::filter::{EnvFilter, LevelFilter};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::{self, fmt};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+    tracing_subscriber::registry()
+        .with(fmt::layer().with_file(true).with_line_number(true))
+        .with(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .init();
+    // tracing_subscriber::fmt().init();
+
+    info!(key = "value", "Hello");
 
     HttpServer::new(|| {
         App::new()
@@ -19,6 +34,7 @@ async fn main() -> std::io::Result<()> {
             .service(delete)
             .service(users)
             .wrap(Logger::default())
+            .wrap(TracingLogger::default())
     })
     .bind(("127.0.0.1", 8080))?
     .run()
